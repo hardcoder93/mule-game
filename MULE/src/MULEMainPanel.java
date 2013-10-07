b9ee99a0aac1e59235d0823f0905d26d18ad1fde
cajenks23 <cajenks23@e1842fd3-93fc-160b-427f-c3a0ca4fbd4e>
2013-10-07 21:34:34 +0000

    added comments and javadocs

--- MULE/src/MULEMainPanel.java
+++ MULE/src/MULEMainPanel.java
@@ -10,28 +10,34 @@ import javax.swing.JPanel;
  * MULEMainPanel is the main panel in the game which controls the current 
  * display in the JFrame. 
  * 
- * @author Chris
- *
+ * @author Chris Jenkins
  */
 @SuppressWarnings("serial")
 public class MULEMainPanel extends JPanel{
 	//Instance Data
 	MULEGameEngine engine;
 	
-	private StartScreen startPanel = new StartScreen();
-	private GameSetup gameSetupPanel = new GameSetup();
-	private PlayerSetup playerSetupPanel = new PlayerSetup();
-	private GameplayPanel gameplayPanel = new GameplayPanel();
-	private CardLayout cardLayout = new CardLayout();
+	private StartScreen startPanel = new StartScreen(); //game startup screen.
+	private GameSetup gameSetupPanel = new GameSetup(); //The panel that allows choice of game type.
+	private PlayerSetup playerSetupPanel = new PlayerSetup(); //The panel that allows each player to make a character.
+	private GameplayPanel gameplayPanel = new GameplayPanel(); //The panel which displays main gameplay.
+	private CardLayout cardLayout = new CardLayout(); //CardLayout allows us to easily switch between screens.
 	
+	//The screen IDs are used to tell the CardLayout which screen to display, and to tell the 
+	//button listeners which button was pressed.
 	private final String startID = "START";
 	private final String gameSetupID = "GAMESETUP";
 	private final String playerSetupID = "PLAYERSETUP";
 	private final String gameplayID = "GAMEPLAY";
 	
+	/**
+	 * Constructs a MULEMainPanel with a set size, adds the 4 game screens, 
+	 * and assigns listeners to the necessary buttons.
+	 */
 	public MULEMainPanel(){
 		setLayout(cardLayout);
 		setPreferredSize(new Dimension(915,587));
+		
 		
 		add(startPanel, startID);
 		add(gameSetupPanel, gameSetupID);
@@ -45,10 +51,14 @@ public class MULEMainPanel extends JPanel{
 		startBtn.addActionListener(new NextListener(startID));		
 		gameSetupBtn.addActionListener(new NextListener(gameSetupID));
 		playerSetupBtn.addActionListener(new NextListener(playerSetupID));
-		
-		//cardLayout.show(startPanel, startID);
 	}
-	
+	/**
+	 * The NextListener class creates button listeners for the "Next" buttons
+	 * on the menu screens, allowing users to switch between screens and 
+	 * start the game.
+	 * 
+	 * @author Chris Jenkins
+	 */
 	private class NextListener implements ActionListener{
 		String ID;
 		
@@ -58,17 +68,17 @@ public class MULEMainPanel extends JPanel{
 		
 		public void actionPerformed(ActionEvent e){
 			switch(ID){
-			case startID:
+			case startID: //If the start button is pressed, display the game setup screen.
 				cardLayout.show(MULEMainPanel.this, gameSetupID);
 				break;
-			case gameSetupID:
+			case gameSetupID: //If the game setup button is pressed, create game engine and show player screen.
 				engine = new MULEGameEngine(gameSetupPanel.getDifficulty(), 
 											gameSetupPanel.getMapType(), 
 											gameSetupPanel.getPlayerCount());
 				playerSetupPanel.setPlayerNumber(engine.getNextPlayerSlot() + 1);
 				cardLayout.show(MULEMainPanel.this, playerSetupID);
 				break;
-			case playerSetupID:
+			case playerSetupID: //If player button is pressed, check that fields are filled, then go to next or game screen.
 				if (playerSetupPanel.getPlayerName().equals("")){
 					playerSetupPanel.showNoInputLabel();
 					break;
