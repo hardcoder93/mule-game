0874d16d37fe27984105ae1fe61e1fe632edef21
cajenks23 <cajenks23@e1842fd3-93fc-160b-427f-c3a0ca4fbd4e>
2013-12-06 19:32:43 +0000

    connected image to background

--- MULE/src/HowToPlayScreen.java
+++ MULE/src/HowToPlayScreen.java
@@ -1,6 +1,11 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,15 +23,23 @@ public class HowToPlayScreen extends JPanel {
 		
 		JLabel backgroundLabel = new JLabel();
 		backgroundLabel.setBounds(0,0,900,600);
-		backgroundLabel.setIcon(new ImageIcon("IMAGES/instruction.jpg"));
-	
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("IMAGES/instruction.jpg"));
+		} catch (IOException e) {
+		    //e.printStackTrace();
+		}
+		Image dimg = img.getScaledInstance(backgroundLabel.getWidth(),
+				backgroundLabel.getHeight(), Image.SCALE_SMOOTH);
+		backgroundLabel.setIcon(new ImageIcon(dimg));
+		//backgroundLabel.setIcon(new ImageIcon("IMAGES/instruction.jpg"));
+		add(backgroundLabel);
 		
 		btnToStartScreen = new JButton("Back to Main Menu");
 		btnToStartScreen.setFont(new Font("Narkisim", Font.BOLD, 15));
 		btnToStartScreen.setBounds(682, 555, 200, 30);
 		btnToStartScreen.setBackground(new Color(255, 255, 255, 150));
 		add(btnToStartScreen);
-		add(backgroundLabel);
 	}
 	
 	public JButton getToStartScreenButton(){
